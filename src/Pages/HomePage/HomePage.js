ba9ac3e6a679e67337f4dbe2aa0bb989e1a769f9
vgrosado <Vgrosado04@gmail.com>
2023-08-27 18:33:14 -0400

    Created HomePage

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,22 @@
+import '../HomePage/HomePage.scss'
+
+function HomePage() {
+    return (
+        <main className='homepage'>
+            <section className='homepage__container'>
+                <div className='homepage__container-1'>
+                    <div className='homepage__artist-div'></div>
+                    <div className='homepage__artist-div'></div>
+                    <div className='homepage__artist-div'></div>
+                </div>
+                <div className='homepage__container-2'>
+                    <div className='homepage__artist-div'></div>
+                    <div className='homepage__artist-div'></div>
+                    <div className='homepage__artist-div'></div>
+                </div>
+            </section>
+        </main>
+    )
+};
+
+export default HomePage;
