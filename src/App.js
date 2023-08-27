7d91a625bdfb4b3b31c9115c227bc6af7ff31ec4
vgrosado <Vgrosado04@gmail.com>
2023-08-27 18:33:14 -0400

    Created HomePage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import SignUpPage from './Pages/SignUpPage/SignUpPage';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LoginPage/>} />
       <Route path="/LoginPage" element={<LoginPage/>} />
       <Route path="/SignUpPage" element={<SignUpPage/>} />
+      <Route path="/HomePage" element={<HomePage/>} />
     </Routes>
   </BrowserRouter>
   );
