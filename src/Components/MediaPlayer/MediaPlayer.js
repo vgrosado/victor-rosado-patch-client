1a398ceebfa1f4a35c290368fa99a8e8a6f42145
vgrosado <Vgrosado04@gmail.com>
2023-09-01 17:06:38 -0400

    fixed rend issues in useprofile, media player work

--- src/Components/MediaPlayer/MediaPlayer.js
+++ src/Components/MediaPlayer/MediaPlayer.js
@@ -1,10 +1,40 @@
 import '../MediaPlayer/MediaPlayer.scss';
+import {AiOutlinePlayCircle, AiOutlinePauseCircle} from 'react-icons/ai'
+import {BiSkipPreviousCircle, BiSkipNextCircle} from 'react-icons/bi'
+import { useState } from 'react';
 
-function MediaPlayer() {
+function MediaPlayer({artist}) {
+    console.log(artist)
+ 
+
+    const [isplaying, setisPlaying] = useState(false);
+    const togglePlay = () => {
+        setisPlaying(!isplaying)
+
+    }
+
+    const artistMusic = artist.music;
+    
+    if (!artistMusic) {
+        return <p>Loading</p>
+    } 
+
     return (
-        <section className='mediaplayer'>
+       
+        <section className='mediaplayer'> 
             <div className='mediaplayer__card'>
-                
+            {artistMusic.map((track)=> (
+        <div key={track.id}> <p key={track.id} className='mediaplayer__song-title'>{track.title}</p> 
+        <audio key={track.id} controls typeof='audio/mp3' src={track.track}/>
+        </div>
+    
+       ))}
+                <div className='mediaplayer__icon-div'>
+                    <BiSkipPreviousCircle className='mediaplayer__icons'/>
+                    {isplaying ? (<AiOutlinePauseCircle className='mediaplayer__play-pause' onClick={togglePlay}/>)
+                    : (<AiOutlinePlayCircle className='mediaplayer__play-pause' onClick={togglePlay}/>) }
+                    <BiSkipNextCircle className='mediaplayer__icons'/>
+                </div>
             </div>
         </section>
     )
