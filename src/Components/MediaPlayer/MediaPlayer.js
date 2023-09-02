726365b9c71339b93aa668700cddbf101c2586da
vgrosado <Vgrosado04@gmail.com>
2023-09-02 19:14:41 -0400

    media player functional

--- src/Components/MediaPlayer/MediaPlayer.js
+++ src/Components/MediaPlayer/MediaPlayer.js
@@ -1,40 +1,75 @@
 import '../MediaPlayer/MediaPlayer.scss';
-import {AiOutlinePlayCircle, AiOutlinePauseCircle} from 'react-icons/ai'
-import {BiSkipPreviousCircle, BiSkipNextCircle} from 'react-icons/bi'
-import { useState } from 'react';
+import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai'
+import { BiSkipPrevious, BiSkipNext } from 'react-icons/bi'
+import { useState, useEffect, useRef } from 'react';
+import { collection, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
+import { db } from '../../Firebase';
+import { useParams } from 'react-router-dom';
 
-function MediaPlayer({artist}) {
-    console.log(artist)
- 
+function MediaPlayer({ music }) {
 
-    const [isplaying, setisPlaying] = useState(false);
+
+    // useEffect(() => {
+    //     const musicRef = doc(db, "Music", "1")
+    //     getDoc(musicRef)
+    //     .then((doc) => {
+    //         setMusic(doc.data())
+    //         console.log(music)
+    //     })
+    //     .catch(error => {
+    //         console.log('error fetching video ID:s', error)
+    //     });
+    // }, [])
+
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef();
     const togglePlay = () => {
-        setisPlaying(!isplaying)
-
+        setIsPlaying(!isPlaying)
     }
 
-    const artistMusic = artist.music;
-    
-    if (!artistMusic) {
+    useEffect(() => {
+        if (audioRef.current) { // Ensure audioRef.current is defined
+            if (isPlaying) {
+                audioRef.current.play();
+            } else {
+                audioRef.current.pause();
+            }
+        }
+    }, [isPlaying]);
+
+    if (!music || music.length === 0) {
         return <p>Loading</p>
-    } 
+    }
 
     return (
-       
-        <section className='mediaplayer'> 
+
+        <section className='mediaplayer'>
             <div className='mediaplayer__card'>
-            {artistMusic.map((track)=> (
+                <div className='mediaplayer__song-div'>
+                    <p className='mediaplayer__song-title'>{music[0].title}</p>
+                    <p className='mediaplayer__song-subtitle'>{music[0].artist}</p>
+                </div>
+                <div className="mediaplayer__song-subtitle">
+                    <span className="mediaplayer__song-subtitle"></span>
+                    <input ref={progressBarRef} 
+                        type="range" 
+                        defaultValue="0"
+                        onChange={handleProgressChange}/>
+                    <span className="mediaplayer__song-subtitle"></span>
+                </div>
+                <audio ref={audioRef} type='audio/mp3' src={music[0].track}></audio>
+                <div className='mediaplayer__icon-div'>
+                    <BiSkipPrevious className='mediaplayer__icons' />
+                    {isPlaying ? (<AiFillPauseCircle className='mediaplayer__play-pause' onClick={togglePlay} />)
+                        : (<AiFillPlayCircle className='mediaplayer__play-pause' onClick={togglePlay} />)}
+                    <BiSkipNext className='mediaplayer__icons' />
+                </div>
+                {/* {artistMusic.map((track)=> (
         <div key={track.id}> <p key={track.id} className='mediaplayer__song-title'>{track.title}</p> 
         <audio key={track.id} controls typeof='audio/mp3' src={track.track}/>
         </div>
-    
-       ))}
-                <div className='mediaplayer__icon-div'>
-                    <BiSkipPreviousCircle className='mediaplayer__icons'/>
-                    {isplaying ? (<AiOutlinePauseCircle className='mediaplayer__play-pause' onClick={togglePlay}/>)
-                    : (<AiOutlinePlayCircle className='mediaplayer__play-pause' onClick={togglePlay}/>) }
-                    <BiSkipNextCircle className='mediaplayer__icons'/>
-                </div>
+        ))} */}
             </div>
         </section>
     )
