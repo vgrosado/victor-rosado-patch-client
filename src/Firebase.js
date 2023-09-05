983a4415a2a976fb466d1bea706ffe480c0aa121
vgrosado <Vgrosado04@gmail.com>
2023-09-04 23:48:04 -0400

    Readme created, .envsample and review from under

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDFfDqHek8HcrYetw_Di-0eEnnu_CJU6s0",
   authDomain: "patch-397bb.firebaseapp.com",
@@ -14,6 +12,6 @@ const firebaseConfig = {
   appId: "1:789116654686:web:54b054c4b9a35d1cb050cf"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
