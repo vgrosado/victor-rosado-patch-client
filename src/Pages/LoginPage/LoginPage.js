87a2e54d5525cb79316625d8b2f24b074be7dc4c
vgrosado <Vgrosado04@gmail.com>
2023-09-07 05:23:12 -0400

    restyled login and signup pages

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,9 +1,8 @@
 import '../LoginPage/LoginPage.scss'
-import backgroundVid from '../../Assets/Videos/LoginBackground3.mp4'
+// import backgroundVid from '../../Assets/Videos/LoginBackground3.mp4'
 import LoginForm from '../../Components/LoginForm/LoginForm'
 import {useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import SignUpForm from '../../Components/SignUpForm/SignUpForm';
+
 
 
 
@@ -11,39 +10,20 @@ import SignUpForm from '../../Components/SignUpForm/SignUpForm';
 function LoginPage() {
 
 	const navigateTo = useNavigate();
-	// function handleLogin(e){
-	// 	e.preventDefault();
-	// 	setLoginForm(true)
-	// 	setSignUpForm(false)
-	// }
-	
-
-	// function handleSignUp(e){
-	// 	e.preventDefault();
-	// 	setLoginForm(false)
-	// 	setSignUpForm(true)
-	// }
-
-
-	function HandleLogin(){
-		navigateTo('/Home');
-	}
-	
-
 	function HandleSignUp(){
 		navigateTo('/SignUp');
 	}
 
 	return (
 		<section className="login">
-			<video src={backgroundVid} id="background-video" autoPlay playsInline loop muted />
+			{/* <video src={backgroundVid} id="background-video" autoPlay playsInline loop muted /> */}
 			<div className="login__container">
 				<h1 className='login__header'>P<span className='login__flicker'>A</span>TCH</h1>
 				<div className='login__subheader-div'>
-					{/* <h2 className='login__subheader'>Make the connection.</h2> */}
+					<LoginForm />
 					<div className='login__switch-container'>
-						<button onClick={HandleLogin}  className='login__button-1' id='switch-button'>Login</button>
-						<button onClick={HandleSignUp} className='login__button-2' id='switch-button'>Register</button>
+						{/* <button onClick={HandleLogin}  className='login__button-1' id='switch-button'>Login</button> */}
+						<p onClick={HandleSignUp} className='login__button-2' id='switch-button'>New to patch? Register now.</p>
 					</div>
 				</div>
 			</div>
