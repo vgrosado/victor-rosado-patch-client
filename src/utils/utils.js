bf6f590175fe641258cd21a4d39afc1650aa5ec4
VICTOR ROSADO <vgrosado04@gmail.com>
2026-08-16 15:51:42 -0400

    EXP-10 fixed paths, updated utils and created global.css

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-variants";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build", // CRA's default build output
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
