d80e17d0e71d36eee5364c9b7b3011dc8b25661a
VICTOR ROSADO <vgrosado04@gmail.com>
2026-08-16 13:36:49 -0400

    EXP-11 adjusted environment variables

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "@firebase/firestore";
-import {getStorage} from "firebase/storage";
-import { createUserWithEmailAndPassword, onAuthStateChanged, getAuth, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  getAuth,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useEffect, useState } from "react";
-const APIKEY = process.env.REACT_APP_FIREBASE_API_KEY;
-const AUTHDOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const PROJECTID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const STORAGEBUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const MESSENGERSENDERID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const APPID = process.env.REACT_APP_FIREBASE_APP_ID;
+const APIKEY = import.meta.VITE_APP_FIREBASE_API_KEY;
+const AUTHDOMAIN = import.meta.VITE_APP_FIREBASE_AUTH_DOMAIN;
+const PROJECTID = import.meta.VITE_APP_FIREBASE_PROJECT_ID;
+const STORAGEBUCKET = import.meta.VITE_APP_FIREBASE_STORAGE_BUCKET;
+const MESSENGERSENDERID = import.meta.VITE_APP_FIREBASE_MESSAGING_SENDER_ID;
+const APPID = import.meta.VITE_APP_FIREBASE_APP_ID;
 
 const firebaseConfig = {
   apiKey: APIKEY,
@@ -16,9 +22,8 @@ const firebaseConfig = {
   projectId: PROJECTID,
   storageBucket: STORAGEBUCKET,
   messagingSenderId: MESSENGERSENDERID,
-  appId: APPID
+  appId: APPID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
@@ -28,25 +33,24 @@ export const auth = getAuth(app);
 
 //login exisisting user
 export function login(email, password) {
-   return signInWithEmailAndPassword(auth, email, password);
-};
+  return signInWithEmailAndPassword(auth, email, password);
+}
 
 //signout current user
-export function logOut(){
-  return signOut(auth)
-};
+export function logOut() {
+  return signOut(auth);
+}
 
 //create/auth new user using email/password
 export function signUp(email, password) {
-   return createUserWithEmailAndPassword(auth, email, password);
-};
+  return createUserWithEmailAndPassword(auth, email, password);
+}
 
-
-export function useAuth(){
+export function useAuth() {
   const [currentUser, setCurrentUser] = useState();
   useEffect(() => {
-    const unSub = onAuthStateChanged(auth, user  => setCurrentUser(user))
+    const unSub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
     return unSub;
-  },[])
+  }, []);
   return currentUser;
-};
+}
